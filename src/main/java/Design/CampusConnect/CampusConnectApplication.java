83ae808757a78ae9a920dd0e198ca9aa17d9ff0c
Jacob Abbott <jaa661@nyu.edu>
2018-10-15 15:21:04 -0400

    trashs setup
    trash setup

---
+++ src/main/java/Design/CampusConnect/CampusConnectApplication.java
@@ -0,0 +1,12 @@
+package Design.CampusConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampusConnectApplication.class, args);
+	}
+}
