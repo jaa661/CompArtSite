926c8052d4897bd615f784262620a3d43661df76
Jacob Abbott <jaa661@nyu.edu>
2018-10-15 20:48:18 -0400

    basic crud
    added crud, but thew database connection breaks

--- src/main/java/Design/CampusConnect/CampusConnectApplication.java
+++ src/main/java/Design/CampusConnect/CampusConnectApplication.java
@@ -8,5 +8,6 @@ public class CampusConnectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CampusConnectApplication.class, args);
+		System.out.println("hello world");
 	}
 }
