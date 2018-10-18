c875c20b6e76b26a575c4dbee71f4a246e4a0552
Jacob Abbott <jaa661@nyu.edu>
2018-10-17 21:10:48 -0400

    alright alright alright
    I unbroke it

--- src/main/java/Design/CampusConnect/entity/Students.java
+++ src/main/java/Design/CampusConnect/entity/Students.java
@@ -1,9 +1,11 @@
 package Design.CampusConnect.entity;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Students {
     @Id
     @GeneratedValue(strategy= GenerationType.AUTO)
