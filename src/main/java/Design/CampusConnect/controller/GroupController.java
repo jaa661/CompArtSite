9c40a6bc0f21575c17e196afd6f9e2e151e4fbfc
Jacob Abbott <jaa661@nyu.edu>
2018-11-27 12:53:45 -0500

    feed update
    feed update

--- src/main/java/Design/CampusConnect/controller/GroupController.java
+++ src/main/java/Design/CampusConnect/controller/GroupController.java
@@ -4,20 +4,18 @@ import Design.CampusConnect.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class FeedController {
+public class GroupController {
 
     @Autowired
     PostService service;
-    
-    @Autowired
-    PostService service;
 
-    @RequestMapping(value = "/feed")
-    public String messages(Model model) {
-        model.addAttribute("feed", service.getAllPosts());
+    @RequestMapping(value = "/group/{groupId}")
+    public String messages(Model model,@PathVariable int groupId) {
+        model.addAttribute("feed", service.getPostsByGroupId(groupId));
         return "mainfeed";
     }
 }
